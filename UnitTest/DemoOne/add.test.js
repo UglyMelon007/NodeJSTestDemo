c52ed25151da58057c341017d652478b81750e57
UglyMelon007 <UglyMelon007@gmail.com>
2016-08-10 14:37:42 +0800

    init

---
+++ UnitTest/DemoOne/add.test.js
@@ -0,0 +1,8 @@
+var add = require('../../DemoOne/add.js');
+var expect = require('chai').expect;
+
+describe('加法测试', function() {
+	it('结果为两参数之和', function() {
+		expect(add(1, 1)).to.be.equal(2);
+	});
+});

522463a199bc1f457eebf9abb7d51e80c066ee98
UglyMelon007 <UglyMelon007@gmail.com>
2016-08-12 17:33:32 +0800

    express improve

--- ExpressDemo/app.js
+++ ExpressDemo/app.js
@@ -11,11 +11,13 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+//
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -55,6 +57,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+app.listen(3000)
 
 
 module.exports = app;

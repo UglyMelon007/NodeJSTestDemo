338dbe73daeb4ac854c5df51624a6bd53bafc5f5
UglyMelon007 <UglyMelon007@gmail.com>
2016-08-15 17:34:04 +0800

    template engine alter

--- ExpressDemo/app.js
+++ ExpressDemo/app.js
@@ -11,13 +11,11 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-//
-app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -57,8 +55,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-app.listen(3000)
-
+app.listen(3000);
 
 module.exports = app;
